9a07cbfdfeedb83b852915a9f455e07eafd030e3
Mahin Hasan <mahinmtrs@gmail.com>
2023-04-16 00:42:58 +0600

    fixed cart update issue

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -15,17 +15,33 @@ const Shop = () => {
     }, []);
     useEffect(() => {
         const storedCart = getStoredCart();
-        console.log(storedCart);
+        const savedCart = [];
         for (const id in storedCart) {
             const addedProduct = products.find(product => product.id === id);
-            console.log(addedProduct);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
         }
+        setCart(savedCart);
     }, [products]);
 
-    const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+    const handleAddToCart = (selectedProduct) => {
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id)
+            exists.quantity = exists.quantity +1;
+            newCart = [...rest, exists];
+        }
+        // const newCart = [...cart, selectedProduct];
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     }
     return (
         <div className='shop-container'>
